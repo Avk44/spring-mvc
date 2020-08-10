4b62e067172efc639d3766602b9b86a53ddd0d43
Abdulvahap Karabekmez <abdulvahap.karabekmez@gmail.com>
2020-08-10 22:42:21 +0200

    initial commit

---
+++ src/main/java/com/avk44/spring/mvc/controller/view/demo/HomeController.java
@@ -0,0 +1,15 @@
+package com.avk44.spring.mvc.controller.view.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	
+	@RequestMapping("/")
+	public String showPage() {
+		return "main-menu";
+	}
+
+}
